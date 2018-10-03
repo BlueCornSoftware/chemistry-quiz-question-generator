d573ee989b2e0dc5fa0ce72e6b458638b4f4dc0e
JakeTheCorn <JakeTheCorn@gmail.com>
2018-10-02 20:41:33 -0400

    adds more iteration for guid function

--- src/lib/guid.test.js
+++ src/lib/guid.test.js
@@ -5,7 +5,7 @@ describe('Function: guid()', () => {
   it('should return a unique string', () => {
     const table = {};
     let u;
-    [ ...Array(100) ].forEach(() => {
+    [ ...Array(1000) ].forEach(() => {
       u = guid();
       expect(table[u]).toBeFalsy();
       table[u] = true;
